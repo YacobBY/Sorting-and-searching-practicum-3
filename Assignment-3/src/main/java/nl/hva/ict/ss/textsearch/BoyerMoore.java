02c67644bc3212f24766f77294407a5455bf4c73
Unknown <yacob101@gmail.com>
2019-01-23 18:02:58 +0100

    bayer moore reversed, nu search nog

--- Assignment-3/src/main/java/nl/hva/ict/ss/textsearch/BoyerMoore.java
+++ Assignment-3/src/main/java/nl/hva/ict/ss/textsearch/BoyerMoore.java
@@ -1,34 +1,54 @@
-/******************************************************************************
- *  Compilation:  javac BoyerMoore.java
- *  Execution:    java BoyerMoore pattern text
- *  Dependencies: StdOut.java
- *
- *  Reads in two strings, the pattern and the input text, and
- *  searches for the pattern in the input text using the
- *  bad-character rule part of the Boyer-Moore algorithm.
- *  (does not implement the strong good suffix rule)
- *
- *  % java BoyerMoore abracadabra abacadabrabracabracadabrabrabracad
- *  text:    abacadabrabracabracadabrabrabracad
- *  pattern:               abracadabra
- *
- *  % java BoyerMoore rab abacadabrabracabracadabrabrabracad
- *  text:    abacadabrabracabracadabrabrabracad
- *  pattern:         rab
- *
- *  % java BoyerMoore bcara abacadabrabracabracadabrabrabracad
- *  text:    abacadabrabracabracadabrabrabracad
- *  pattern:                                   bcara
- *
- *  % java BoyerMoore rabrabracad abacadabrabracabracadabrabrabracad
- *  text:    abacadabrabracabracadabrabrabracad
- *  pattern:                        rabrabracad
- *
- *  % java BoyerMoore abacad abacadabrabracabracadabrabrabracad
- *  text:    abacadabrabracabracadabrabrabracad
- *  pattern: abacad
- *
- ******************************************************************************/
+/**
+ * Compilation:  javac BoyerMoore.java
+ * Execution:    java BoyerMoore pattern text
+ * Dependencies: StdOut.java
+ * <p>
+ * Reads in two strings, the pattern and the input text, and
+ * searches for the pattern in the input text using the
+ * bad-character rule part of the Boyer-Moore algorithm.
+ * (does not implement the strong good suffix rule)
+ * <p>
+ * % java BoyerMoore abracadabra abacadabrabracabracadabrabrabracad
+ * text:    abacadabrabracabracadabrabrabracad
+ * pattern:               abracadabra
+ * <p>
+ * % java BoyerMoore rab abacadabrabracabracadabrabrabracad
+ * text:    abacadabrabracabracadabrabrabracad
+ * pattern:         rab
+ * <p>
+ * % java BoyerMoore bcara abacadabrabracabracadabrabrabracad
+ * text:    abacadabrabracabracadabrabrabracad
+ * pattern:                                   bcara
+ * <p>
+ * % java BoyerMoore rabrabracad abacadabrabracabracadabrabrabracad
+ * text:    abacadabrabracabracadabrabrabracad
+ * pattern:                        rabrabracad
+ * <p>
+ * % java BoyerMoore abacad abacadabrabracabracadabrabrabracad
+ * text:    abacadabrabracabracadabrabrabracad
+ * pattern: abacad
+ * <p>
+ * <p>
+ * The {@code BoyerMoore} class finds the first occurrence of a pattern string
+ * in a text string.
+ * <p>
+ * This implementation uses the Boyer-Moore algorithm (with the bad-character
+ * rule, but not the strong good suffix rule).
+ * <p>
+ * For additional documentation,
+ * see <a href="https://algs4.cs.princeton.edu/53substring">Section 5.3</a> of
+ * <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ * <p>
+ * The {@code BoyerMoore} class finds the first occurrence of a pattern string
+ * in a text string.
+ * <p>
+ * This implementation uses the Boyer-Moore algorithm (with the bad-character
+ * rule, but not the strong good suffix rule).
+ * <p>
+ * For additional documentation,
+ * see <a href="https://algs4.cs.princeton.edu/53substring">Section 5.3</a> of
+ * <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ */
 
 /**
  *  The {@code BoyerMoore} class finds the first occurrence of a pattern string
@@ -50,6 +70,7 @@ public class BoyerMoore {
     private int[] movementArray;     // the bad-character skip array
 
     private String pat;      // or as a string
+
     /**
      * Preprocesses the pattern string.
      *
@@ -64,38 +85,23 @@ public class BoyerMoore {
         // position of rightmost occurrence of c in the pattern
         movementArray = new int[R];
         //Set every possible character to -1
-        for (int c = 0; c < R; c++)
+        for (int c = 0; c < R; c++) {
             movementArray[c] = -1;
+        }
+        int reverseCounter = 0;
         //Set every character to the number of letters it's last occurance from the first pattern character
-        for (int j = 0; j < pat.length(); j++)
-            movementArray[pat.charAt(j)] = j;
+        for (int j = pat.length() - 1; j >= 0; j--) {
+            movementArray[pat.charAt(j)] = reverseCounter;
+            System.out.println("letter " + pat.charAt(j) + " value = " + movementArray[pat.charAt(j)]);
+            reverseCounter++;
+        }
+//        for (int c = 0; c < R; c++) {
+//            if (movementArray[c] != -1) {
+//                System.out.println(movementArray[c]);
+//            }
+//        }
     }
 
-    /**
-     * Returns the index of the first occurrrence of the pattern string
-     * in the text string.
-     *
-     * @param  txt the text string
-     * @return the index of the first occurrence of the pattern string
-     *         in the text string; n if no such match
-     */
-    public int search(String txt) {
-        int m = pat.length();
-        int n = txt.length();
-        int skip;
-        for (int i = 0; i <= n - m; i += skip) {
-            skip = 0;
-            for (int j = m-1; j >= 0; j--) {
-                if (pat.charAt(j) != txt.charAt(i+j)) {
-                    searchComparisons++;
-                    skip = Math.max(1, j - movementArray[txt.charAt(i+j)]);
-                    break;
-                }
-            }
-            if (skip == 0) return i;    // found
-        }
-        return n;                       // not found
-    }
     /**
      * Takes a pattern string and an input string as command-line arguments;
      * searches for the pattern string in the text string; and prints
@@ -119,5 +125,31 @@ public class BoyerMoore {
         System.out.println(pat);
         System.out.println();
         System.out.println(" Total comparisons: " + searchComparisons);
+    }
+
+    /**
+     * Returns the index of the first occurrrence of the pattern string
+     * in the text string.
+     *
+     * @param  txt the text string
+     * @return the index of the first occurrence of the pattern string
+     *         in the text string; n if no such match
+     */
+    public int search(String txt) {
+        int m = pat.length();
+        int n = txt.length();
+        int skip;
+        for (int i = 0; i <= n - m; i += skip) {
+            skip = 0;
+            for (int j = m - 1; j >= 0; j--) {
+                if (pat.charAt(j) != txt.charAt(i + j)) {
+                    searchComparisons++;
+                    skip = Math.max(1, j - movementArray[txt.charAt(i + j)]);
+                    break;
+                }
+            }
+            if (skip == 0) return i;    // found
+        }
+        return n;                       // not found
     }
 }
