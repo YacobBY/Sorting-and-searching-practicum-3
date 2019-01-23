562fbde18e06240adbfc0bfe585abb0ba1fa4b7d
Unknown <yacob101@gmail.com>
2019-01-23 16:37:06 +0100

    removed duplicate char method

--- Assignment-3/src/main/java/nl/hva/ict/ss/textsearch/BoyerMoore.java
+++ Assignment-3/src/main/java/nl/hva/ict/ss/textsearch/BoyerMoore.java
@@ -49,12 +49,7 @@ public class BoyerMoore {
     private final int R;     // the radix
     private int[] right;     // the bad-character skip array
 
-    private char[] pattern;  // store the pattern as a character array
     private String pat;      // or as a string
-
-
-
-
     /**
      * Preprocesses the pattern string.
      *
@@ -75,26 +70,6 @@ public class BoyerMoore {
     }
 
     /**
-     * Preprocesses the pattern string.
-     *
-     * @param pattern the pattern string
-     * @param R the alphabet size
-     */
-    public BoyerMoore(char[] pattern, int R) {
-        this.R = R;
-        this.pattern = new char[pattern.length];
-        for (int j = 0; j < pattern.length; j++)
-            this.pattern[j] = pattern[j];
-
-        // position of rightmost occurrence of c in the pattern
-        right = new int[R];
-        for (int c = 0; c < R; c++)
-            right[c] = -1;
-        for (int j = 0; j < pattern.length; j++)
-            right[pattern[j]] = j;
-    }
-
-    /**
      * Returns the index of the first occurrrence of the pattern string
      * in the text string.
      *
@@ -108,9 +83,7 @@ public class BoyerMoore {
         int skip;
         for (int i = 0; i <= n - m; i += skip) {
             skip = 0;
-            searchComparisons++;
             for (int j = m-1; j >= 0; j--) {
-                searchComparisons++;
                 if (pat.charAt(j) != txt.charAt(i+j)) {
                     searchComparisons++;
                     skip = Math.max(1, j - right[txt.charAt(i+j)]);
@@ -121,36 +94,6 @@ public class BoyerMoore {
         }
         return n;                       // not found
     }
-
-
-    /**
-     * Returns the index of the first occurrrence of the pattern string
-     * in the text string.
-     *
-     * @param  text the text string
-     * @return the index of the first occurrence of the pattern string
-     *         in the text string; n if no such match
-     */
-    public int search(char[] text) {
-        int m = pattern.length;
-        int n = text.length;
-        int skip;
-        for (int i = 0; i <= n - m; i += skip) {
-            searchComparisons++;
-            skip = 0;
-            for (int j = m-1; j >= 0; j--) {
-                searchComparisons++;
-                if (pattern[j] != text[i+j]) {
-
-                    skip = Math.max(1, j - right[text[i+j]]);
-                    break;
-                }
-            }
-            if (skip == 0) return i;    // found
-        }
-        return n;                       // not found
-    }
-
     /**
      * Takes a pattern string and an input string as command-line arguments;
      * searches for the pattern string in the text string; and prints
@@ -161,27 +104,18 @@ public class BoyerMoore {
     public static void main(String[] args) {
         String pat = "lolxd";
         String txt = "aaaaaaalolxd";
-        char[] pattern = pat.toCharArray();
-        char[] text    = txt.toCharArray();
 
         BoyerMoore boyermoore1 = new BoyerMoore(pat);
-        BoyerMoore boyermoore2 = new BoyerMoore(pattern, 256);
+
         int offset1 = boyermoore1.search(txt);
-        int offset2 = boyermoore2.search(text);
 
         // print results
         System.out.println("text:    " + txt);
-
         System.out.print("pattern: ");
         for (int i = 0; i < offset1; i++)
             System.out.print(" ");
         System.out.println(pat);
-
-        System.out.print("pattern: ");
-        for (int i = 0; i < offset2; i++)
-            System.out.print(" ");
-        System.out.println(pat);
         System.out.println();
-        System.out.println(searchComparisons);
+        System.out.println(" Total comparisons: " + searchComparisons);
     }
 }
