4695c1f1a5a0f0364f142e7da29112b30bde3dde
Unknown <yacob101@gmail.com>
2019-02-03 13:24:07 +0100

    languageList done

--- Assignment-3/src/main/java/nl/hva/ict/ss/textsearch/LanguageList.java
+++ Assignment-3/src/main/java/nl/hva/ict/ss/textsearch/LanguageList.java
@@ -8,19 +8,19 @@ public class LanguageList {
     public LanguageList() {
         //source: https://en.wikipedia.org/wiki/Letter_frequency
         languageList.add(new Language("English", "etaoinshrdlcumwfgypbvkjxqzüäößàâáåã"));
-        languageList.add(new Language("French", "esaitnruoldcmpvéqfbghjàxzèêyçkûùâwî "));
-        languageList.add(new Language("German", "ensriatdhulgcomwbfkzüvpäößjyxqéàèêç "));
-        languageList.add(new Language("Spanish", "eaosrnidltcmupbgvyqóíhfájzéñxúwükäö "));
-        languageList.add(new Language("Portuguese", "eaosrnidltcmupbgvyqóíhfájzéñxúwükäö "));
-        languageList.add(new Language("Esperanto", "eaosrnidltcmupbgvyqóíhfájzéñxúwükäö "));
-        languageList.add(new Language("Italian", "eaosrnidltcmupbgvyqóíhfájzéñxúwükäö "));
-        languageList.add(new Language("Turkish", "eaosrnidltcmupbgvyqóíhfájzéñxúwükäö "));
-        languageList.add(new Language("Polish", "eaosrnidltcmupbgvyqóíhfájzéñxúwükäö "));
-        languageList.add(new Language("Dutch", "eaosrnidltcmupbgvyqóíhfájzéñxúwükäö "));
-        languageList.add(new Language("Icelandic", "eaosrnidltcmupbgvyqóíhfájzéñxúwükäö "));
-        languageList.add(new Language("Finnish", "eaosrnidltcmupbgvyqóíhfájzéñxúwükäö "));
-        languageList.add(new Language("Czech", "eaosrnidltcmupbgvyqóíhfájzéñxúwükäö "));
-
+        languageList.add(new Language("French", "esaitnruoldcmpvéqfbghjàxzèêyçkûùâwî"));
+        languageList.add(new Language("German", "ensriatdhulgcomwbfkzüvpäößjyxqéàèêç"));
+        languageList.add(new Language("Spanish", "eaosrnidltcmupbgvyqóíhfájzéñxúwükäö"));
+        languageList.add(new Language("Portuguese", "aeosridmntculpvgqbfhãôâçzêjéóxúíáàõ"));
+        languageList.add(new Language("Esperanto", "aieonlsrtkjudmpvgfbcĝĉŭzŝhĵĥqãôâçêé"));
+        languageList.add(new Language("Italian", "eaionlrtscdpumvgzfbhàqèúwíyjkxòĝĉŭŝ"));
+        languageList.add(new Language("Turkish", "aeinrlkdımytusobüşzcghçğvpöfjàqèúwí"));
+        languageList.add(new Language("Swedish", "eanrtsildomkgvhfupäbcåöyjxwzqıüşçğà"));
+        languageList.add(new Language("Polish", "aieonwrszcdyklmtpłujbgóęhśćżąńfźvxä"));
+        languageList.add(new Language("Dutch", "enatirodslgvhkmubpwjzcfxyqłóęśćżąńź"));
+        languageList.add(new Language("Danish", "erntaidslogkmfvbuphåøæjycwzxqóíáéñú"));
+        languageList.add(new Language("Icelandic", "arniestulðgmkfvohádíþjbóyæpöéúýxåøc"));
+        languageList.add(new Language("Finnish", "aintesloukämrvjhpydögbcfwzxqåðáíþóæ"));
+        languageList.add(new Language("Czech", "aeonitvsrldkmupízjhěyýábcžšéčřůgfúx"));
     }
-
 }
