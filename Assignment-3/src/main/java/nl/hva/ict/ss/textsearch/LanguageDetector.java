99f1641e6b3ae9a590bf0b0b7a6661fc2e5d91a6
Unknown <yacob101@gmail.com>
2019-02-03 12:44:39 +0100

    Working character counter ?

--- Assignment-3/src/main/java/nl/hva/ict/ss/textsearch/LanguageDetector.java
+++ Assignment-3/src/main/java/nl/hva/ict/ss/textsearch/LanguageDetector.java
@@ -7,32 +7,54 @@ import java.util.regex.Pattern;
 
 public class LanguageDetector {
     private String content; // Once an instance is created this will hold the complete content of the file.
+    private String code;
+    private String docs;
+    private int letterCount[];
 
     public LanguageDetector(InputStream input) {
         Scanner sc = new Scanner(input);
         sc.useDelimiter("\\Z"); // EOF marker
         content = (sc.next());
-        String code  = content.toLowerCase();
+        separateDocsFromCode();
+        alphabetCounter();
+    }
 
-        StringBuilder docs = new StringBuilder();
-
-        Pattern commentFinder = Pattern.compile("(?s)/\\*(.)*?\\*/",Pattern.MULTILINE | Pattern.COMMENTS );
+    public void separateDocsFromCode() {
+        code = content.toLowerCase();
+        StringBuilder docsBuilder = new StringBuilder();
+        Pattern commentFinder = Pattern.compile("(?s)/\\*(.)*?\\*/", Pattern.MULTILINE | Pattern.COMMENTS);
         Matcher matcher = commentFinder.matcher(code);
         Matcher matcher2 = commentFinder.matcher(code);
-
         while (matcher.find()) {
-            docs.append(matcher.group());
+            docsBuilder.append(matcher.group());
         }
         while (matcher2.find()) {
             code = matcher.replaceAll("");
         }
-
-        System.out.println(docs);
-        System.out.println("-----------------------------");
-        System.out.println(code);
-
+        docs = docsBuilder.toString();
+//        System.out.println(docs);
+//        System.out.println("-----------------------------");
+//        System.out.println(code);
     }
 
+    public void alphabetCounter() {
+        letterCount = new int[123];
+        for (int i = 0; i < 123; i++) {
+            letterCount[i] = 0; // -1 for chars not in pattern
+            System.out.println(letterCount[i]);
+        }
+        System.out.println("----------------------");
+
+        for (int i = 'a'; i <= 'z'; i++) {
+            Pattern letterPattern = Pattern.compile("" + (char) i, Pattern.MULTILINE | Pattern.COMMENTS);
+            Matcher letterMatcher = letterPattern.matcher(docs);
+
+            while (letterMatcher.find()) {
+                letterCount[i]++;
+            }
+            System.out.println(letterCount[i]);
+        }
+    }
 }
-
-
+//a = 97
+//z = 122
